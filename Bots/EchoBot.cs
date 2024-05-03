73fcace940bbf99105bfba0274e08f00d96a2766
Ridhita Banerjee <ridhita.banerjee@willistowerswatson.com>
2024-05-03 15:27:09 +0530

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,40 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.22.0
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var activityText = $"{turnContext.Activity.Text}";
+            var replyText = "";
+            if (activityText == "how are you?")
+            {
+                replyText = "I am good what about you";
+            }
+            else
+            {
+                replyText = activityText;
+            }
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
